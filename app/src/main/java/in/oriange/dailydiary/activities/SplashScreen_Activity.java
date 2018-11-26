ba347e0ebef8393c6b600eb26c9eb191a33f700e
sk_avantecodeworx <shrikant.kulkarni@avantecodeworx.com>
2018-11-26 15:28:12 +0530

    Initial Commit

---
+++ app/src/main/java/in/oriange/dailydiary/activities/SplashScreen_Activity.java
@@ -0,0 +1,23 @@
+package in.oriange.dailydiary.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import in.oriange.dailydiary.R;
+
+public class SplashScreen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
