de1203056828a8a54c2fee1bb9974d03e8097c45
priyeshpawar369 <priyeshpawar07@gmail.com>
2018-11-30 01:17:36 +0530

    Added fragment transactions for navigation and integrated banner api in Home Fragment

--- app/src/main/java/in/oriange/dailydiary/activities/MainDrawer_Activity.java
+++ app/src/main/java/in/oriange/dailydiary/activities/MainDrawer_Activity.java
@@ -2,32 +2,31 @@ package in.oriange.dailydiary.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.FrameLayout;
 
-import com.daimajia.slider.library.Animations.DescriptionAnimation;
-import com.daimajia.slider.library.SliderLayout;
-import com.daimajia.slider.library.SliderTypes.BaseSliderView;
-import com.daimajia.slider.library.SliderTypes.TextSliderView;
-import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.luseen.spacenavigation.SpaceItem;
 import com.luseen.spacenavigation.SpaceNavigationView;
 import com.luseen.spacenavigation.SpaceOnClickListener;
 
-import java.util.HashMap;
-
 import in.oriange.dailydiary.R;
 import in.oriange.dailydiary.fragments.BottomSheetMenu_Fragment;
+import in.oriange.dailydiary.fragments.Home_Fragment;
+import in.oriange.dailydiary.fragments.Notification_Fragment;
+import in.oriange.dailydiary.fragments.Shopping_Fragment;
+import in.oriange.dailydiary.fragments.Time_Fragment;
 
-public class MainDrawer_Activity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
+public class MainDrawer_Activity extends AppCompatActivity {
 
     private Context context;
     private SpaceNavigationView bottom_navigation;
-
+    private FrameLayout fl_fragmentlayout;
     public static final String TAG = "bottom_sheet";
 
-    private SliderLayout mDemoSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +35,7 @@ public class MainDrawer_Activity extends AppCompatActivity implements BaseSlider
 
         init();
         setBottomNavigation();
+        setDefaults();
         setEventListner();
         setUpToolbar();
 
@@ -43,41 +43,8 @@ public class MainDrawer_Activity extends AppCompatActivity implements BaseSlider
 
     private void init() {
         bottom_navigation = findViewById(R.id.bottom_navigation);
-        mDemoSlider = (SliderLayout) findViewById(R.id.slider);
+        fl_fragmentlayout = findViewById(R.id.fl_fragmentlayout);
 
-        HashMap<String, String> url_maps = new HashMap<String, String>();
-        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
-        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
-        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
-        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
-
-//        HashMap<String, Integer> file_maps = new HashMap<String, Integer>();
-//        file_maps.put("Hannibal", R.drawable.hannibal);
-//        file_maps.put("Big Bang Theory", R.drawable.hannibal);
-//        file_maps.put("House of Cards", R.drawable.hannibal);
-//        file_maps.put("Game of Thrones", R.drawable.hannibal);
-
-        for (String name : url_maps.keySet()) {
-            TextSliderView textSliderView = new TextSliderView(this);
-            // initialize a SliderLayout
-            textSliderView
-                    .description(name)
-                    .image(url_maps.get(name))
-                    .setScaleType(BaseSliderView.ScaleType.Fit)
-                    .setOnSliderClickListener(this);
-
-            //add your extra information
-            textSliderView.bundle(new Bundle());
-            textSliderView.getBundle()
-                    .putString("extra", name);
-
-            mDemoSlider.addSlider(textSliderView);
-        }
-        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
-        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
-        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
-        mDemoSlider.setDuration(4000);
-        mDemoSlider.addOnPageChangeListener(this);
     }
 
     private void setBottomNavigation() {
@@ -92,6 +59,15 @@ public class MainDrawer_Activity extends AppCompatActivity implements BaseSlider
         bottom_navigation.showIconOnly();
     }
 
+    private void setDefaults() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fl_fragmentlayout, new Home_Fragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
+
     private void setEventListner() {
         bottom_navigation.setSpaceOnClickListener(new SpaceOnClickListener() {
             @Override
@@ -101,7 +77,36 @@ public class MainDrawer_Activity extends AppCompatActivity implements BaseSlider
 
             @Override
             public void onItemClick(int itemIndex, String itemName) {
-                Log.d("onItemClick ", "" + itemIndex + " " + itemName);
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                switch (itemIndex) {
+                    case 0:
+                        fragmentTransaction.replace(R.id.fl_fragmentlayout, new Home_Fragment());
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        break;
+
+                    case 1:
+                        fragmentTransaction.replace(R.id.fl_fragmentlayout, new Shopping_Fragment());
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        break;
+
+                    case 2:
+                        fragmentTransaction.replace(R.id.fl_fragmentlayout, new Notification_Fragment());
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        break;
+
+                    case 3:
+                        fragmentTransaction.replace(R.id.fl_fragmentlayout, new Time_Fragment());
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        break;
+
+                }
+
             }
 
             @Override
@@ -117,22 +122,8 @@ public class MainDrawer_Activity extends AppCompatActivity implements BaseSlider
     }
 
     @Override
-    public void onSliderClick(BaseSliderView slider) {
-
-    }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
--- app/src/main/java/in/oriange/dailydiary/fragments/Home_Fragment.java
+++ app/src/main/java/in/oriange/dailydiary/fragments/Home_Fragment.java
@@ -1,6 +1,177 @@
 package in.oriange.dailydiary.fragments;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Home_Fragment extends Fragment {
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import in.oriange.dailydiary.R;
+import in.oriange.dailydiary.utilities.ApplicationConstants;
+import in.oriange.dailydiary.utilities.Utilities;
+import in.oriange.dailydiary.utilities.WebServiceCalls;
+
+public class Home_Fragment extends Fragment implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
+
+    private Context context;
+    private SliderLayout mDemoSlider;
+    private CoordinatorLayout main_content;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        context = getActivity();
+        init(rootView);
+        getSessionData();
+        setDefault();
+        setEventHandlers();
+        return rootView;
+    }
+
+    private void init(View rootView) {
+        mDemoSlider = rootView.findViewById(R.id.slider);
+        main_content = getActivity().findViewById(R.id.main_content);
+
+    }
+
+    private void getSessionData() {
+
+    }
+
+    private void setDefault() {
+
+        if (Utilities.isInternetAvailable(context)) {
+            new GetBanners().execute();
+        } else {
+            Utilities.showSnackBar(main_content, "Please Check Internet Connection");
+        }
+
+    }
+
+    private void setEventHandlers() {
+
+    }
+
+    private void setSlider(JSONArray bannerJsonArray) {
+
+        for (int i = 0; i < bannerJsonArray.length(); i++) {
+            try {
+                JSONObject jsonObject = bannerJsonArray.getJSONObject(i);
+
+                TextSliderView textSliderView = new TextSliderView(context);
+                textSliderView
+                        .description(jsonObject.getString("BannerText"))
+                        .image(jsonObject.getString("BannerImagePath"))
+//                        .image("http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg")
+                        .setScaleType(BaseSliderView.ScaleType.Fit)
+                        .setOnSliderClickListener(this);
+
+                textSliderView.bundle(new Bundle());
+                textSliderView.getBundle().putString("extra", jsonObject.getString("BannerText"));
+
+                mDemoSlider.addSlider(textSliderView);
+                mDemoSlider.setCurrentPosition(0, true);
+                mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+                mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+                mDemoSlider.setDuration(5000);
+                mDemoSlider.movePrevPosition();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+//        for (String name : url_maps.keySet()) {
+//            TextSliderView textSliderView = new TextSliderView(context);
+//            textSliderView
+//                    .description(name)
+//                    .image(url_maps.get(name))
+//                    .setScaleType(BaseSliderView.ScaleType.Fit)
+//                    .setOnSliderClickListener(this);
+//
+//            textSliderView.bundle(new Bundle());
+//            textSliderView.getBundle().putString("extra", name);
+//
+//            mDemoSlider.addSlider(textSliderView);
+//
+//            mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+//            mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+//            mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+//            mDemoSlider.setDuration(5000);
+//            mDemoSlider.addOnPageChangeListener(this);
+//        }
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    public class GetBanners extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String res = "[]";
+            JsonObject obj = new JsonObject();
+            obj.addProperty("type", "getBanners");
+            res = WebServiceCalls.JSONAPICall(ApplicationConstants.banners, obj.toString());
+            return res.trim();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            String type = "", message = "";
+            try {
+                if (!result.equals("")) {
+                    JSONObject mainObj = new JSONObject(result);
+                    type = mainObj.getString("type");
+                    message = mainObj.getString("message");
+                    if (type.equalsIgnoreCase("success")) {
+                        JSONArray jsonarr = mainObj.getJSONArray("data");
+                        if (jsonarr.length() > 0) {
+                            setSlider(jsonarr);
+
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
